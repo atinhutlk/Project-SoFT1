e538ed6953f1e0cc90ba28d989ded7dc6ed6fb72
atinhutlk <nhut.vo@metropolia.fi>
2023-12-06 22:32:01 +0200

    Merge remote-tracking branch 'origin/main'

--- JVtest.js
+++ JVtest.js
@@ -1,70 +1,83 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Constants
     const startMoney = 1000;
     const startRange = 2000;
-    let startAirportICAO = "LFPG"; // Replace with your starting airport's ICAO code
+    const startAirportICAO = "LFPG";
 
-    // Initial game state
     let money = startMoney;
     let range = startRange;
     let currentAirportICAO = startAirportICAO;
 
-    // Sample array of airports in range (replace this with your actual data)
-    const airportsInRange = [
-        { name: "Airport 1", icao: "ICAO1" },
-        { name: "Airport 2", icao: "ICAO2" },
-        // Add more airports as needed
-    ];
-
-    // HTML elements
     const moneyDisplay = document.querySelector(".money");
     const rangeDisplay = document.querySelector(".range");
     const playerNameInput = document.getElementById("screen-name");
     const startButton = document.getElementById("start-button");
     const buyButton = document.getElementById("buy-button");
-    const airportsList = document.getElementById("airports-list");
-    const destinationSelect = document.getElementById("icao-code");
+    const airportsList = document.querySelector(".game-destination ul");
+    const destinationInput = document.getElementById("icao-code");
     const submitDestinationButton = document.getElementById("submit-icao-button");
+    const secretBoxButtons = document.querySelector(".game-secret-box-buttons");
+    const restartButton = document.getElementById("restart-button");
 
-    // Event listeners
     startButton.addEventListener("click", startGame);
     buyButton.addEventListener("click", buyFuel);
     submitDestinationButton.addEventListener("click", submitDestination);
+    secretBoxButtons.addEventListener("click", handleSecretBox);
+    restartButton.addEventListener("click", restartGame);
 
-    // Functions
     function startGame(event) {
         event.preventDefault();
         const playerName = playerNameInput.value;
         if (playerName) {
             alert(`Welcome, ${playerName}! Your adventure begins at ${startAirportICAO}.`);
             showElement("game-resource-update");
-            document.getElementById("startAirportICAO").textContent = startAirportICAO;
-            showAirportsInRange();
+            getAirports(); // Call the function to get airports
         } else {
             alert("Please enter a player name.");
         }
     }
 
-    function showAirportsInRange() {
-        airportsList.innerHTML = ""; // Clear the existing list
-
-        airportsInRange.forEach(airport => {
-            const listItem = document.createElement("li");
-            listItem.textContent = `${airport.name} (ICAO: ${airport.icao})`;
-            airportsList.appendChild(listItem);
-
-            // Add each airport to the destination select dropdown
-            const option = document.createElement("option");
-            option.value = airport.icao;
-            option.textContent = `${airport.name} (ICAO: ${airport.icao})`;
-            destinationSelect.appendChild(option);
-        });
+    async function getAirports() {
+        try {
+            const response = await fetch('/get_airports', { method: 'GET' });
+            if (!response.ok) {
+                throw new Error('Error retrieving airports');
+            }
+            const airports = await response.json();
+            showAirportsInRange(airports);
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
     }
 
-    function buyFuel(event) {
+    function showAirportsInRange(airports) {
+        airportsList.innerHTML = "";
+
+        airports.forEach(airport => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${airport.name} (ICAO: ${airport.ident})`;
+            airportsList.appendChild(listItem);
+        });
+        showElement("game-destination");
+    }
+
+    async function buyFuel(event) {
         event.preventDefault();
         const fuelAmount = parseFloat(document.getElementById("resource-exchange").value);
         if (!isNaN(fuelAmount) && fuelAmount >= 0) {
+            const response = await fetch('/buy_fuel', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ fuelAmount }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Error buying fuel');
+            }
+
+            const result = await response.json();
+
             range += fuelAmount * 2;
             money -= fuelAmount;
             updateDisplay();
@@ -74,19 +87,73 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    function submitDestination(event) {
+    async function submitDestination(event) {
         event.preventDefault();
-        const selectedDestination = destinationSelect.value;
+        const selectedDestination = destinationInput.value;
         if (selectedDestination) {
-            // Perform logic for submitting the destination
-            alert(`You have selected ${selectedDestination} as your destination.`);
-            // Update game state or perform other actions as needed
+            const response = await fetch('/submit_destination', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ selectedDestination }),
+            });
 
-            // Move to the next step
+            if (!response.ok) {
+                throw new Error('Error submitting destination');
+            }
+
+            alert(`You have selected ${selectedDestination} as your destination.`);
             showElement("game-secret-box");
         } else {
-            alert("Please select a valid destination.");
+            alert("Please enter a valid destination.");
         }
+    }
+
+    async function handleSecretBox(event) {
+        const clickedButton = event.target.id;
+        if (clickedButton === "money") {
+            const response = await fetch('/handle_money_button', { method: 'GET' }); // Adjust the endpoint based on your server
+            if (!response.ok) {
+                throw new Error('Error handling money button');
+            }
+
+            // Handle the case when the player chooses 100 EUR
+            // Replace this comment with your logic
+            alert("Player chose 100 EUR. Implement your logic here.");
+        } else if (clickedButton === "range") {
+            const response = await fetch('/handle_range_button', { method: 'GET' }); // Adjust the endpoint based on your server
+            if (!response.ok) {
+                throw new Error('Error handling range button');
+            }
+
+            // Handle the case when the player chooses 50 km
+            // Replace this comment with your logic
+            alert("Player chose 50 km. Implement your logic here.");
+        } else if (clickedButton === "skip") {
+            const response = await fetch('/handle_skip_button', { method: 'GET' }); // Adjust the endpoint based on your server
+            if (!response.ok) {
+                throw new Error('Error handling skip button');
+            }
+
+            // Handle the case when the player skips
+            // Replace this comment with your logic
+            alert("Player skipped. Implement your logic here.");
+        }
+    }
+
+    async function restartGame() {
+        const response = await fetch('/restart_game', { method: 'POST' }); // Adjust the endpoint based on your server
+        if (!response.ok) {
+            throw new Error('Error restarting game');
+        }
+
+        // Reset game variables and UI elements
+        money = startMoney;
+        range = startRange;
+        currentAirportICAO = startAirportICAO;
+        updateDisplay();
+        showElement("intro");
     }
 
     function showElement(elementClass) {
